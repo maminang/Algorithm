b0ded5570e77d0bd86b10cb039a2a4c582017d6b
Hamin <Hamin@DESKTOP-9OGN2GF>
2020-03-13 19:41:11 +0900

    왜 또 지랄이야

--- Algorithm/src/backjoon/HanoiTower.java
+++ Algorithm/src/backjoon/HanoiTower.java
@@ -12,20 +12,21 @@ public class HanoiTower { // 11729
 	public static void main(String[] args) {
 		int n = sc.nextInt();
 
-		System.out.println(move(n, 0, 1, 2));
 		try {
+			bw.write((int) (Math.pow(2, n) - 1) + "\n");
+			move(n, 0, 1, 2);
 			bw.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	static int move(int n, int from, int by, int to) { // n개의 원반을 from 장대에서 by 장대를 거쳐 to 장대로 cnt번 옮김
-		int cnt = 0;
+	static long move(int n, int from, int by, int to) { // n개의 원반을 from 장대에서 by 장대를 거쳐 to 장대로 옮기고 몇번 옮겼는지 반환
+		long cnt = 0;
 		if (n > 1) {
-			cnt += move(n - 1, from, to, by);
-			cnt += move(1, from, by, to);
-			cnt += move(n - 1, by, from, to);
+			cnt += move(n - 1, from, to, by); // n-1 개를 by에 옮기고
+			cnt += move(1, from, by, to); // 맨밑에 있던 원반 하나를 to로 옮기고
+			cnt += move(n - 1, by, from, to); // by에 있던 n-1개의 원반을 to로 옮긴다
 		} else {
 			try {
 				bw.write((from + 1) + " " + (to + 1) + "\n");
